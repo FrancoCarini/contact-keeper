fd1139ab4354c967ec594d53fd17b27c6fa1636f
Franco Carini <carini.franco@gmail.com>
2021-03-28 11:06:49 -0300

    Added Front with Context API

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p className="my-1">Contact Keeper</p>
+      <p className="bg-dark p"><strong>V1.0.0</strong></p>
+    </div>
+  )
+}
+
+export default About
